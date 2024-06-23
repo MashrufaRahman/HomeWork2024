938d01af8c05b1716956b84c44c8c427b565ef4d
Mashrufa Rahman <mashrfrhmn@gmail.com>
2024-06-23 13:58:39 -0400

    update6_23_2024

---
+++ src/hw7Inheritance/Birds.java
@@ -0,0 +1,7 @@
+package hw7Inheritance;
+//Animal is the parent class and Birds is the child class
+public class Birds extends Animal {
+public void birdsInfo() {
+	System.out.println("This method is from Birds Class");
+}
+}
